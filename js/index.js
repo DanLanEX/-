e79c3017ecd4f70add23e1e4d9a7c4f53d1d0bd9
danlanex <qas87@126.com>
2018-07-09 17:28:51 +0800

    提交文件

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// 引入“蒙纳正圆简”字体
+$youziku.load("body", "d8c325a49c2e4699b4c3143c3324dbe6", "MYuenPRC-SemiBold");
+   /*$youziku.load("#id1,.class1,h1", "d8c325a49c2e4699b4c3143c3324dbe6", "MYuenPRC-SemiBold");*/
+   /*．．．*/
+   $youziku.draw();
+
+// 登录事件弹窗
+function Login() {
+    alert("后端暂未完工，登录不可用，喵喵喵");
+}
